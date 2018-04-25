2f37aa0aea514a400a4cdcc87b9a8a7fd8415a3f
Metin Senturk <metinsenturk@me.com>
2018-04-25 15:41:09 -0400

    update project directory

--- test.py
+++ test.py
@@ -1,42 +1,66 @@
-import boto3
+import decimal
+import psycopg2 as psy
+from psycopg2.extras import RealDictCursor, DictCursor, NamedTupleCursor
+import json
+import csv
 
-a_c_c_e_s_s__k_e_y = 'AKIAIBBRXQ4FUMMEHIEQ'
-s_e_c_r_e_t__k_e_y = 'LGlXz4iUWjHYsl7zI9uWVNXDy0FYkT92tfgTKTy4'
-r_e_g_i_o_n__n_a_m_e = 'us-east-1'
 
-dynamodb = boto3.client('dynamodb',
-      endpoint_url='http://localhost:8000/')
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    raise TypeError
 
-response = dynamodb.list_tables(
-    ExclusiveStartTableName='string',
-    Limit=10
-)
-items = response['TableNames']
-print(items)
 
-response = dynamodb.create_table()
+# connection
+s = "host='localhost' user='postgres' dbname='zagi'"
+psy_conn = psy.connect(s)
 
-"""
-# inserting into a table
-rq = requests.request('GET', 'https://randomuser.me/api/')
-user_data = rq.json()['results']
+print(psy_conn.get_dsn_parameters())
+print(psy_conn.closed)
 
-item = {
-    'gender': {
-        'S': user_data[0]['gender'],
-    },
-    'cell': {
-        'S': user_data[0]['cell'],
-    },
-    'phone': {
-        'S': user_data[0]['phone'],
-    },
-    'registered': {
-        'S': user_data[0]['registered'],
-    }
-}
+# cursor
+cursor = psy_conn.cursor(cursor_factory=RealDictCursor)
+print(cursor.closed)
+print(cursor.name)
+print(cursor.description)
 
-user_inserted = dydb.insert_item('users', item)
-print(user_inserted)
-print(user_inserted)
-"""
+# cursor operations in db
+cursor.execute("select version();")
+cursor.execute("select * from product;")
+rows = cursor.fetchone()
+print(json.dumps(rows, default=decimal_default))
+
+print("---------------------------------")
+
+cursor2 = psy_conn.cursor(cursor_factory=NamedTupleCursor)
+cursor2.execute("select * from product;")
+rows = cursor2.fetchone()
+print(rows)
+
+print("---------------------------------")
+
+cursor3 = psy_conn.cursor(cursor_factory=DictCursor)
+cursor3.execute("select * from product;")
+rows = cursor3.fetchone()
+print(rows)
+
+print("---------------------------------")
+
+cursor4 = psy_conn.cursor()
+cursor4.execute("select * from product;")
+rows = cursor4.fetchall()
+print(rows)
+
+with open("product.csv", 'w') as f:
+    writer = csv.writer(f, delimiter=',')
+    for row in rows:
+        writer.writerow(row)
+
+# closing cursor and connection
+cursor.close()
+cursor2.close()
+cursor3.close()
+cursor4.close()
+psy_conn.close()
+print(cursor.closed)
+print(psy_conn.closed)
